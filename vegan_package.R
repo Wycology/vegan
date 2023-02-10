615a11b4efe87617f0866f5009492a1cd9668857
Wycology <wyclifeoluoch@gmail.com>
2023-02-10 21:30:32 +0300

    Following the vegan documentation

--- vegan_package.R
+++ vegan_package.R
@@ -3,7 +3,7 @@ library(vegan) # Version 2.6.4
 data("varespec") # Data sets of cover by lichen species
 data("varechem") # Corresponding environmental parameters
 
-ord <- metaMDS(varespec)
+ord <- metaMDS(varespec) # Ordination of the species
 
 plot(ord, type = "t")
 
