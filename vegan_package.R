c05ecbd0fd486ea8fbd3ed621d4912949b085c48
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2023-07-24 21:35:46 +0200

    Update vegan_package.R

--- vegan_package.R
+++ vegan_package.R
@@ -5,7 +5,7 @@ library(vegan) # Version 2.6.4
 data("varespec") # Datasets of cover by lichen specis
 data("varechem") # Corresponding environmental parameters
 
-ord <- metaMDS(varespec, distance = "bray", k = 2) # Ordination of the sps
+ord <- metaMDS(varespec, distance = "bray", k = 2) # Ordination of the spps
 
 plot(ord, type = "t") # Showing the plot 
 plot(ord, type = "p")
